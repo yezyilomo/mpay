a106334666c7ef20e41d98bef62acdf39a755955
yezy <yezileliilomo@hotmail.com>
2017-07-31 14:59:05 +0300

    Updating controller with post http request handler

--- controllers/example.py
+++ controllers/example.py
@@ -1,6 +1,18 @@
-from odoo import http
+from odoo import http, api
+from odoo.http import request
 
 class Example(http.Controller):
-    @http.route('/example', type='http', auth='public', website=True)
+    @http.route('/auth_usr', type='http', auth='public', website=True, )
     def render_example_page(self):
-        return http.request.render("Mpay.example__page", {'hell': "Yezileli",'conf': True})
+        return http.request.render("Mpay.example__page", {})
+
+    @http.route('/method', type='http', auth='public', website=True, methods=['POST'])
+
+    def post_method(self, **kw):
+        data=request.env['received.transaction'].search([],limit=10)
+        for i in data:
+            print(i.sender_name)
+        print ( '\n'+ kw['username']+'\n')
+        print ( '\n'+ kw['password']+'\n')
+        print ( '\n'+ kw['message']+'\n')
+        return request.render("Mpay.posted_page",{'data':data})
