a84a8f316ffe25fc3e671951a989da3e849c189f
yezy <yezileliilomo@hotmail.com>
2017-08-08 20:31:56 +0300

    Modifying models, controllers and handling error in Payment confirmation

--- controllers/controllers.py
+++ controllers/controllers.py
@@ -12,28 +12,36 @@ class Controller(http.Controller):
     @http.route('/get_transaction_sms', type='http', auth='public', website=True, methods=['POST'])
     def post_method(self, **sms):
         #Here we have to extract important information from SMS sent and store to received.transaction model
-        data=request.env['received.transaction'].search([],limit=10)
+        data=request.env['received.transaction'].sudo().search([],limit=10)
         return "Info Stored"
 
     @http.route('/paynow', type='http', auth='public', website=True, methods=['POST'])
     def pay_now(self, **kw):
         data=kw
-        return request.render("Mpay.transaction_id_fill",{'amount': data['amount'], 'currency': data['currency']})
+        error=""
+        return request.render("Mpay.transaction_id_fill",{'amount': data['amount'], 'currency': data['currency'],'error': error})
 
     @http.route('/validate', type='http', auth='public', website=True, methods=['POST','GET'])
     def validate_payment(self, **kw):
-        transaction=request.env['received.transaction'].search([('transaction_id','=',kw['transaction_id'] ) ],limit=1)
+        data=kw;
+        transaction=request.env['received.transaction'].sudo().search([('transaction_id','=',kw['transaction_id'] ) ],limit=1)
         if not transaction.exists() :  # if invalid transaction_id is provided
-            return "No transaction"
+            #error="No transaction"
+            error="Invalid transaction Id, try again!."
+            return request.render("Mpay.transaction_id_fill",{'amount': data['amount'], 'currency': data['currency'], 'error': error})
 
         if transaction.transaction_status == 'done' :  # if provided transaction_id has already been used to confirm transaction
-           return "Transaction has already been done"
+           #error="Transaction has already been done"
+           error="Invalid transaction Id, try again!."
+           return request.render("Mpay.transaction_id_fill",{'amount': data['amount'], 'currency': data['currency'], 'error': error})
 
         if transaction.transaction_status == 'pending' : # if transaction is in pending state not yet confirmed
 
-           if transaction.received_amount != kw['amount'] : # if paid amount is not equal to required amount
+           if float(transaction.received_amount) != float(kw['amount']) : # if paid amount is not equal to required amount
               transaction.transaction_status='incomplete'
-              return "You have paid insufficient money"
+              error= "You have paid insufficient money"
+              return request.render("Mpay.transaction_id_fill",{'amount': data['amount'], 'currency': data['currency'], 'error': error})
 
            transaction.transaction_status='done'  #Everything is right now we confirm payment
-           return "Thank you, You have done your payment successfully, Wait for shipment of your products"
+           error="none"
+           return request.render("Mpay.transaction_id_fill",{'amount': data['amount'], 'currency': data['currency'], 'error': error})
--- models/models.py
+++ models/models.py
@@ -9,26 +9,22 @@ class MpayAcquirer(models.Model):
         res['tokenize'].append('mpay')
         return res
 
-
 class MpayTransaction(models.Model):
     _inherit = 'payment.transaction'
-
 
 class PaymentService(models.Model):
     _name='payment.service'
     _description='This store Service provider informations'
-
     service_provider=fields.Char('Service Provider', required=True)
     service_number=fields.Char('Service Number', required=True)
-
 
 class ReceivedTransaction(models.Model):
     _name='received.transaction'
     _description='This store all details from synchronised SMS'
-
     sender_name=fields.Char('Sender Name', required=True)
     sender_phone=fields.Char('Sender Phone', required=True)
     transaction_id=fields.Char('Transaction Id', required=True)
     received_amount=fields.Float('Received Amount', required=True)
+    transaction_currency=fields.Char('Currency', required=True)
     service_provider=fields.Char('Service Provider', required=True)
     transaction_status=fields.Selection([('pending','pending'),('done','done'),('incomplete','incomplete')],required=True)
